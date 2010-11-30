d7b51fd27303c980f323fd363675598a903a8777
Neuhaus <tassianors@gmail.com>
2010-11-30 00:31:05 -0200

    modelling started

--- matlab_files/simul_iv_old.m
+++ matlab_files/simul_iv_old.m
@@ -17,12 +17,14 @@ STD=0.1;
 tempo = 0:Ts:Tf;
 N=size(tempo, 2);
 
-M=300;
+M=100;
 
 % TFs
 %ARX
-G=tf([2],[1 -0.8 0], Ts);
-H=tf([1 0 0],[1 -0.8 0], Ts);
+% G=tf([2],[1 -0.8 0], Ts);
+% H=tf([1 0 0],[1 -0.8 0], Ts);
+G=tf([2],[1 -0.8], Ts);
+H=tf([1 0],[1 -0.8], Ts);
 
 % Replace the default stream with a stream whose seed is based on CLOCK, so
 % RAND will return different values in different MATLAB sessions
@@ -30,7 +32,7 @@ RandStream.setDefaultStream( RandStream('mt19937ar', 'seed', sum(100*clock)));
 
 % identification using MMQ
 % model y(t)=a*u(t-2)+(b+c)*y(t-1) +bc*y(t-2)
-teta=[2; 0.8];
+teta=[2; 0.8];%; 0];
 n=size(teta, 1);
 % e entrada u saida do controlador
 %phy=[ u(t-2); y(t-1); y(t-2)]
@@ -38,6 +40,7 @@ n=size(teta, 1);
 % numero de vezes que sera aplicado o metodo.
 a=zeros(M,1);
 b=zeros(M,1);
+%c=zeros(M,1);
 for j=1:M
     % make a randon noise with std = 0.1
     ran=rand(N, 1);
@@ -48,12 +51,6 @@ for j=1:M
     % make noise be zero mean
     rh=(ran_s-m)*STD;
 
-%     % make a randon noise with std = 1
-%     ran=rand(N, 1);
-%     s=std(ran);
-%     m=mean(ran);
-%     % now rr has std=1nusoidal input signal
-%     rr=(ran-m)/s;
     %sim
     rr=sin(freq*tempo);
     mean(rr)
@@ -64,28 +61,46 @@ for j=1:M
     u=rr;
     
     phy=zeros(N, n);
-    for t=3:N
-        phy(t, 1)=u(t-2);
+    z=zeros(N, n);
+    for t=2:N
+        phy(t, 1)=u(t-1);
         phy(t, 2)=y(t-1);
     end
 
+%     for t=3:N
+%         phy(t, 1)=u(t-2);
+%         phy(t, 2)=y(t-1);
+%         phy(t, 3)=y(t-2);
+%     end
+    for t=2:N
+        % auxiliary instrument z
+        z(t, 1)=u(t);
+        z(t, 2)=u(t-1);
+    end
+%     for t=4:N
+%         % auxiliary instrument z
+%         z(t, 1)=u(t-1);
+%         z(t, 2)=u(t-2);
+%         z(t, 3)=u(t-3);
+%     end
+
     % make sure, rank(phy) = n :)
-    teta_r=inv(phy'*phy)*phy'*y;
+    teta_r=inv(z'*phy)*z'*y;
     % to be used in grafic plot
     a(j)=teta_r(1);
     b(j)=teta_r(2);
+    %c(j)=teta_r(3);
 end
 PN=[a, b];
 ma=mean(a)
-sa=std(a);
 mb=mean(b)
-
+%mc=mean(c)
 
 plot(a, b, 'bo');
 hold;
 plot(ma, mb, 'rx');
 hold;
-title('Simulacao do sistema para um modelo ARX')
+title('Simulacao do sistema para o metodo das variavies instrumentais')
 xlabel('Valor da estimativa para a variavel a')
 ylabel('Valor da estimativa para a variavel b')
 legend('Estimativas', 'Media')
